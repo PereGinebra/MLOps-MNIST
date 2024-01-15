09f6af7e5ef5c8490b1e43935fefca72fdf16e3f
Pere Ginebra Solanellas <pere.ginebra@gmail.com>
2024-01-16 00:54:24 +0100

    test messages

--- tests/test_data.py
+++ tests/test_data.py
@@ -3,8 +3,8 @@ from numpy import unique
 
 def test_data():
     train, test = mnist()
-    assert len(train) == 30000
-    assert len(test) == 5000
-    assert [(image.shape, type(label.item())) for image, label in train] == [((28,28),int)]*len(train)
-    assert [(image.shape, type(label.item())) for image, label in test] == [((28,28),int)]*len(test)
-    assert unique([label for _, label in train]).tolist() == list(range(10))
+    assert len(train) == 30000, 'Unexpected size for training dataset'
+    assert len(test) == 5000, 'Unexpected size for test dataset'
+    assert [(image.shape, type(label.item())) for image, label in train] == [((28,28),int)]*len(train), 'Unexpected shape for some training datapoint'
+    assert [(image.shape, type(label.item())) for image, label in test] == [((28,28),int)]*len(test), 'Unexpected shape for some testing datapoint'
+    assert unique([label for _, label in train]).tolist() == list(range(10)), 'Missing representation from some label or wrong label set'
