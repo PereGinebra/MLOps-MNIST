335b14a199df8a316a22c6a9f6ae63359a5d5d91
Pere Ginebra Solanellas <pere.ginebra@gmail.com>
2024-01-16 01:02:26 +0100

    skip tests if data is not present

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,6 +1,14 @@
+import os.path
+
+import pytest
+
 from corrupt_mnist.data.dataset import mnist
 from numpy import unique
 
+@pytest.mark.skipif(not os.path.exists('data/processed/train_images.pt'), reason="Train image file not found")
+@pytest.mark.skipif(not os.path.exists('data/processed/train_target.pt'), reason="Train label file not found")
+@pytest.mark.skipif(not os.path.exists('data/processed/test_images.pt'), reason="Test image file not found")
+@pytest.mark.skipif(not os.path.exists('data/processed/test_target.pt'), reason="Test label file not found")
 def test_data():
     train, test = mnist()
     assert len(train) == 30000, 'Unexpected size for training dataset'
